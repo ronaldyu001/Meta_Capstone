ef928a3435da022893d75cae5ee3882ec4711624
ronaldyu001 <ronaldyu001@gmail.com>
2025-06-21 14:26:55 -0500

    - implemented Module 4 tasks

--- MySQL_WB/main.sql
+++ MySQL_WB/main.sql
@@ -95,4 +95,128 @@ BEGIN
 END//
 DELIMITER ;
 
-CALL CancelOrder( 5 )
+CALL CancelOrder( 5 );
+
+
+#
+#	Exercise: Create SQL queries to check available bookings based on user input
+#
+
+# Module 2, Task 1
+DELETE FROM bookings WHERE booking_id > 4;
+
+SELECT * FROM bookings;
+
+UPDATE bookings
+SET booking_date = '2022-10-10', table_no = 5, customer_id = '00-090-3491'
+WHERE booking_id = 1;
+
+UPDATE bookings
+SET booking_date = '2022-11-12', table_no = 3, customer_id = '00-352-9063'
+WHERE booking_id = 2;
+
+UPDATE bookings
+SET booking_date = '2022-10-11', table_no = 2, customer_id = '00-381-6823'
+WHERE booking_id = 3;
+
+UPDATE bookings
+SET booking_date = '2022-10-13', table_no = 2, customer_id = '00-090-3491'
+WHERE booking_id = 4;
+
+
+# Module 2, Task 2
+DROP PROCEDURE IF EXISTS CheckBooking;
+
+DELIMITER //
+CREATE PROCEDURE CheckBooking( IN b_date DATE, IN tbl_no INT, OUT booking_exists BOOLEAN )
+BEGIN
+	SET booking_exists = FALSE;
+    
+    SELECT EXISTS(
+		SELECT TRUE FROM bookings
+		WHERE booking_date = b_date AND table_no = tbl_no)
+	INTO booking_exists;
+    
+	IF booking_exists THEN 
+		SELECT CONCAT('Table ', tbl_no, ' is already booked.') AS 'Booking status';
+    END IF;
+END//
+DELIMITER ;
+
+CALL CheckBooking('2022-11-12', 3);
+
+
+# Module 2, Task 3
+DROP PROCEDURE IF EXISTS AddValidBooking;
+
+DELIMITER //
+CREATE PROCEDURE AddValidBooking( IN b_date DATE, IN tbl_no INT )
+BEGIN
+	DECLARE booking_exists BOOLEAN;
+    CALL CheckBooking(b_date, tbl_no, booking_exists);
+    
+	START TRANSACTION;
+		INSERT INTO bookings (booking_id, booking_date, table_no)
+        SELECT MAX(booking_id) + 1, b_date, tbl_no FROM bookings;
+    
+    IF booking_exists THEN
+		ROLLBACK;
+        SELECT CONCAT('Table ', tbl_no, ' is already booked - booking cancelled') AS 'Booking status';
+	ELSE COMMIT;
+    END IF;
+END//
+DELIMITER ;
+
+CALL AddValidBooking('2022-10-10', 5);
+
+
+#
+#	Exercise: Create SQL queries to add and update bookings
+# 
+
+# Module 2, Task 1
+DROP PROCEDURE IF EXISTS AddBooking;
+
+DELIMITER //
+CREATE PROCEDURE AddBooking ( IN b_id INT, IN c_id VARCHAR(100), IN b_date DATE, IN tbl_no INT )
+BEGIN
+	INSERT INTO bookings (booking_id, customer_id, booking_date, table_no)
+    VALUES(b_id, c_id, b_date, tbl_no);
+    SELECT CONCAT('New booking added;') AS Confirmation;
+END//
+DELIMITER ;
+
+CALL AddBooking(9, '00-090-3491', '2022-12-30', 4);
+
+DELETE FROM Bookings WHERE booking_id = 9;
+SELECT * FROM bookings;
+
+
+# Module 2, Task 2
+DROP PROCEDURE IF EXISTS UpdateBooking;
+
+DELIMITER //
+CREATE PROCEDURE UpdateBooking( IN b_id INT, IN b_date DATE )
+BEGIN
+	UPDATE bookings SET booking_date = b_date WHERE booking_id = b_id;
+    SELECT CONCAT('Booking ', b_id, ' updated.') AS Confirmation;
+END//
+DELIMITER ;
+
+CALL UpdateBooking(9, '2022-12-17');
+
+select * from bookings;
+
+
+# Module 2, Task 3
+DROP PROCEDURE IF EXISTS CancelBooking;
+
+DELIMITER //
+CREATE PROCEDURE CancelBooking( IN b_id INT )
+BEGIN
+	DELETE FROM Bookings WHERE booking_id = b_id;
+    SELECT CONCAT('Booking ', b_id,' cancelled.') AS Confirmation;
+END//
+DELIMITER ;
+
+CALL CancelBooking(9)
